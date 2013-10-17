6bd65615839955ea0110335e41839be1988d4cb7
Andrew Huth <andrew@Andrews-MacBook-Air.local>
2013-10-17 14:23:24 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets */
+
+// Indentator indents each line in the open document according to your current
+// preferences (i.e. tabs vs spaces, 2/4/8 spaces).
+define(function (require, exports, module) {
+    "use strict";
+
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager  = brackets.getModule("editor/EditorManager"),
+        Menus          = brackets.getModule("command/Menus"),
+        COMMAND_ID     = "indentator.autoIndent";
+
+    function autoIndent() {
+        var editor = EditorManager.getFocusedEditor();
+        if (!editor) {
+            return;
+        }
+        var doc = editor._codeMirror,
+            lines = doc.lineCount(),
+            index;
+        for (index = 0; index < lines; index++) {
+            doc.indentLine(index);
+        }
+    }
+    
+    CommandManager.register("Indent Document", COMMAND_ID, autoIndent);
+
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuItem(COMMAND_ID);
+});
